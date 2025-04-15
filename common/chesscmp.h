3313e2141e78860fa972ff40258bc3b6a3954821
neostreet <lloyd.aidan@gmail.com>
2025-04-15 11:46:06 -0400

    Add files via upload

---
+++ common/chesscmp.h
@@ -0,0 +1,18 @@
+#define IDM_NEW                100
+#define IDM_OPEN               101
+#define IDM_SAVE               102
+#define IDM_SAVEAS             103
+#define IDM_EXIT               104
+
+#define IDI_CHESSCMP           105
+#define IDI_SMALL              106
+
+#define IDM_TOGGLE_ORIENTATION 107
+
+#define IDM_ABOUT              108
+
+#define IDC_STATIC -1
+
+#define DLG_VERFIRST        400
+#define IDC_COMPANY        DLG_VERFIRST
+#define IDC_MOVE_NUMBER    DLG_VERFIRST+1
